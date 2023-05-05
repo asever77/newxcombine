b8eec63fc83ba779add81a75e96ed52b3eab3b74
asever77 <johyunmin77@gmail.com>
2023-05-05 11:57:30 +0900

    d

--- resources/js/common.js
+++ resources/js/common.js
@@ -5,7 +5,108 @@ const global = 'uiNXC';
 window[global] = {};
 
 const Global = window[global];
+const UA = navigator.userAgent.toLowerCase();
+const deviceSize = [1920, 1600, 1440, 1280, 1024, 960, 840, 720, 600, 480, 400, 360];
+const deviceInfo = ['android', 'iphone', 'ipod', 'ipad', 'blackberry', 'windows ce', 'windows','samsung', 'lg', 'mot', 'sonyericsson', 'nokia', 'opeara mini', 'opera mobi', 'webos', 'iemobile', 'kfapwi', 'rim', 'bb10'];
 
+Global.state = {
+	device: {
+		info: (() => {
+			for (let i = 0, len = deviceInfo.length; i < len; i++) {
+				if (UA.match(deviceInfo[i]) !== null) {
+					return deviceInfo[i];
+				}
+			}
+		})(),
+		width: window.innerWidth,
+		height: window.innerHeight,
+		breakpoint: null,
+		colClass: null,
+		ios: (/ip(ad|hone|od)/i).test(UA),
+		android: (/android/i).test(UA),
+		app: UA.indexOf('appname') > -1 ? true : false,
+		touch: null,
+		mobile: null,
+		os: (navigator.appVersion).match(/(mac|win|linux)/i)
+	},
+	browser: {
+		ie: UA.match(/(?:msie ([0-9]+)|rv:([0-9\.]+)\) like gecko)/i),
+		local: (/^http:\/\//).test(location.href),
+		firefox: (/firefox/i).test(UA),
+		webkit: (/applewebkit/i).test(UA),
+		chrome: (/chrome/i).test(UA),
+		opera: (/opera/i).test(UA),
+		safari: (/applewebkit/i).test(UA) && !(/chrome/i).test(UA),	
+		size: null
+	},
+	scroll: {
+		y: 0,
+		direction: 'down'
+	},		
+	breakPoint: [600, 905],
+	resizeState() {
+		let timerWin;
+
+		const act = () => {
+			const browser = Global.state.browser;
+			const device = Global.state.device;
+
+			device.width = window.innerWidth;
+			device.height = window.innerHeight;
+
+			device.touch = device.ios || device.android || (document.ontouchstart !== undefined && document.ontouchstart !== null);
+			device.mobile = device.touch && (device.ios || device.android);
+			device.os = device.os ? device.os[0] : '';
+			device.os = device.os.toLowerCase();
+
+			device.breakpoint = device.width >= deviceSize[5] ? true : false;
+			device.colClass = device.width >= deviceSize[5] ? 'col-12' : device.width > deviceSize[8] ? 'col-8' : 'col-4';
+
+			if (browser.ie) {
+				browser.ie = browser.ie = parseInt( browser.ie[1] || browser.ie[2] );
+				( 11 > browser.ie ) ? support.pointerevents = false : '';
+				( 9 > browser.ie ) ? support.svgimage = false : '';
+			} else {
+				browser.ie = false;
+			}
+			
+			const clsBrowser = browser.chrome ? 'chrome' : browser.firefox ? 'firefox' : browser.opera ? 'opera' : browser.safari ? 'safari' : browser.ie ? 'ie' + browser.ie : 'other';
+			const clsMobileSystem = device.ios ? "ios" : device.android ? "android" : 'etc';
+			const clsMobile = device.mobile ? device.app ? 'ui-a ui-m' : 'ui-m' : 'ui-d';
+			const el_html = document.querySelector('html');
+
+			el_html.classList.remove('col-12', 'col-8', 'col-4');
+			el_html.classList.add(device.colClass);
+			el_html.classList.add(clsBrowser);
+			el_html.classList.add(clsMobileSystem);
+			el_html.classList.add(clsMobile);
+		
+			const w = window.innerWidth;
+
+			clearTimeout(timerWin);
+			timerWin = setTimeout(() => {
+				el_html.classList.remove('size-tablet');
+				el_html.classList.remove('size-desktop');
+				el_html.classList.remove('size-mobile');
+					el_html.classList.remove('size-desktop');
+
+				if (w < Global.state.breakPoint[0]) {
+					Global.state.browser.size = 'mobile';
+					el_html.classList.add('size-mobile');
+				} else if (w < Global.state.breakPoint[1]) {
+					Global.state.browser.size = 'tablet';
+					el_html.classList.add('size-tablet');
+				} else {
+					Global.state.browser.sizee = 'desktop';
+					el_html.classList.add('size-desktop');
+				}
+			},200);
+		}
+		window.addEventListener('resize', act);
+		act();
+	}
+}
+Global.state.resizeState();
 Global.scroll = {
 	y : 0,
 	direction : 'down',
